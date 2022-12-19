c32c89c80c61a53653d0537a74263ea603abfdd8
Connor Lim <dev@connorlim.net>
2022-12-19 20:26:46 +0800

    Add event list to home page

--- web/src/components/Event/EventStrip.tsx
+++ web/src/components/Event/EventStrip.tsx
@@ -1,31 +1,29 @@
-import { Card, CardContent, Divider, Stack, Typography } from '@mui/material';
-import { EventData } from '../../services/EventService';
-import UserService from '../../services/UserService';
-import AppEvent from '../../types/event.app';
+import { Card, CardContent, Divider, Stack, Typography } from "@mui/material";
+import { EventData } from "../../services/EventService";
+import UserService from "../../services/UserService";
+import AppEvent from "../../types/event.app";
 
 export interface EventStripProps {
-	event: EventData;
+  event: EventData;
 }
 
 const EventStrip = ({ event }: EventStripProps) => {
-	return (
-		<Card>
-			<CardContent>
-				<Stack direction="column" spacing={1}>
-					<Typography variant="h6">{event.title}</Typography>
-					<Stack
-						direction="row"
-						spacing={1}
-						divider={<Divider orientation="vertical" flexItem />}>
-						{[event.user_id, event.timestamp].map((s) => (
-							<Typography variant="caption">{s}</Typography>
-						))}
-					</Stack>
-					<Typography>{event.description}</Typography>
-				</Stack>
-			</CardContent>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardContent>
+        <Stack direction="column" spacing={1}>
+          <Typography variant="h6">{event.title}</Typography>
+          <Stack direction="row" spacing={1} divider={<Divider orientation="vertical" flexItem />}>
+            <Typography variant="caption">
+              Date: {new Date(parseInt(event.timestamp)).toDateString()}
+            </Typography>
+            <Typography variant="caption">By: {event.user_id}</Typography>
+          </Stack>
+          <Typography>{event.description}</Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default EventStrip;
