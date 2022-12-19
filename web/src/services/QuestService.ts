ac9035a188008bf1a390590520d9334def3bc656
minirei <minrei101@gmail.com>
2022-12-19 15:16:39 +0800

    feat: services boilerplate

---
+++ web/src/services/QuestService.ts
@@ -0,0 +1,27 @@
+// interface 
+
+class QuestService {
+
+  // get all quests
+  public async getQuests() {
+    //
+  }
+
+  // get quest by id
+  public async getQuest() {
+
+  }
+
+  // create quest
+  public async createQuest() {
+    // logic here (something like the below)
+    // const res = firebase.functions.user.getUser()
+
+    // redux update state?
+    // another option is to update redux state via the useEffect that calls this enclosing function
+    
+  }
+
+}
+
+export default QuestService
---
+++ web/src/services/UserService.ts
@@ -0,0 +1,15 @@
+class UserService {
+  // get all users
+  public async getUsers() {
+    //
+  }
+
+  // get user by id
+  public async getUser(id: number) {
+    // logic here (something like the below)
+    // const res = firebase.functions.user.getUser()
+  }
+
+}
+
+export default UserService
