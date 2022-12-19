63e395945545ab11c72a33b9c20e1aee437d4cfb
minirei <minrei101@gmail.com>
2022-12-19 13:48:01 +0800

    feat: added react router

---
+++ web/src/components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+const Layout = (props: LayoutProps) => {
+  return (
+    <div>
+      <Nav />
+        <main>{props.children}</main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
