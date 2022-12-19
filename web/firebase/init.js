2b09f1613b08cf79c0656f67016cb4c8bbade67c
StanleyNeoh <neohstanley@gmail.com>
2022-12-19 14:26:56 +0800

    Initiialised firebase app

---
+++ web/firebase/init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCb5CPxFFvz3_3cfiHuCVmETJYw0RZS9jQ",
+  authDomain: "rcfour-d8147.firebaseapp.com",
+  projectId: "rcfour-d8147",
+  storageBucket: "rcfour-d8147.appspot.com",
+  messagingSenderId: "573895304880",
+  appId: "1:573895304880:web:cef3d20a8d1c0e52abfd75",
+  measurementId: "G-5R053BMDH4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout/Layout"
+import "../firebase/init.js"
 
 // routes
 import HomePage from "./routes/HomePage"
