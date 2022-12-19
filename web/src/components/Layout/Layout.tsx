409a9c9de5c257521df760570d9eb7d6261b8b4e
hansstanley <nusstuhss@gmail.com>
2022-12-19 18:25:37 +0800

    Add snacks slice

--- web/src/components/Layout/Layout.tsx
+++ web/src/components/Layout/Layout.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import Nav from "./Nav";
-import Footer from "./Footer";
+import Nav from './Nav';
+import Footer from './Footer';
 
 type LayoutProps = {
-  children: React.ReactNode
-}
+	children: React.ReactNode;
+};
 
 const Layout = (props: LayoutProps) => {
-  return (
-    <div>
-      <Nav />
-      <main>{props.children}</main>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Nav />
+			<main>{props.children}</main>
+			<Footer />
+		</div>
+	);
+};
 
-export default Layout
+export default Layout;
---
+++ web/src/components/Snackbar/MainSnackbar.tsx
@@ -0,0 +1,47 @@
+import { Alert, Snackbar } from '@mui/material';
+import { SyntheticEvent, useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../features/app/hooks';
+import {
+	onSnackEaten,
+	Snack,
+	takeSnack,
+} from '../../features/snacks/snacksSlice';
+
+const MainSnackbar = () => {
+	const dispatch = useAppDispatch();
+	const { snackInFocus, snacks } = useAppSelector((state) => state.snacks);
+
+	const [open, setOpen] = useState(false);
+
+	useEffect(() => {
+		if (snacks.length && !snackInFocus) {
+			dispatch(takeSnack());
+			setOpen(true);
+		} else if (snacks.length && snackInFocus && open) {
+			setOpen(false);
+		}
+	}, [snacks, snackInFocus, open]);
+
+	const handleClose = (event: SyntheticEvent | Event, reason?: string) => {
+		if (reason === 'clickaway') return;
+		setOpen(false);
+	};
+
+	const handleExited = () => {
+		dispatch(onSnackEaten());
+	};
+
+	return (
+		<Snackbar
+			open={open}
+			autoHideDuration={6000}
+			onClose={handleClose}
+			TransitionProps={{ onExited: handleExited }}>
+			<Alert severity={snackInFocus?.severity} onClose={handleClose}>
+				{snackInFocus?.message}
+			</Alert>
+		</Snackbar>
+	);
+};
+
+export default MainSnackbar;
