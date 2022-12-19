a9061a4fde9e025167d3cc1f26ef979f8a5676e6
StanleyNeoh <neohstanley@gmail.com>
2022-12-19 14:33:24 +0800

    moved firebase

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout/Layout"
-import "../firebase/init.js"
+import "./firebase/init.ts"
 
 // routes
 import HomePage from "./routes/HomePage"
