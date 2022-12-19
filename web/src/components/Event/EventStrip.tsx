f42bc52f19746c02c18eaa4663a607502c112355
hansstanley <nusstuhss@gmail.com>
2022-12-19 15:08:27 +0800

    Create EventStrip.tsx

---
+++ web/src/components/Event/EventStrip.tsx
@@ -0,0 +1,28 @@
+import { Card, CardContent, Divider, Stack, Typography } from '@mui/material';
+import AppEvent from '../../types/event.app';
+
+export interface EventStripProps {
+	event: AppEvent;
+}
+
+const EventStrip = ({ event }: EventStripProps) => {
+	return (
+		<Card>
+			<CardContent>
+				<Stack direction="column" spacing={1}>
+					<Typography variant="h6">{event.title}</Typography>
+					<Stack
+						direction="row"
+						spacing={1}
+						divider={<Divider orientation="vertical" flexItem />}>
+						{[event.organiser, event.timestamp].map((s) => (
+							<Typography variant="caption">{s}</Typography>
+						))}
+					</Stack>
+				</Stack>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default EventStrip;
