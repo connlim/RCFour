7cbf5de9502578369db7f198be3f49e1f5503b82
minirei <minrei101@gmail.com>
2022-12-19 16:17:32 +0800

    chore: new create route

---
+++ web/src/components/Creation/Creation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EventCreation from "./EventCreation";
+import QuestCreation from "./QuestCreation";
+
+const Creation = () => {
+  return (
+    <React.Fragment>
+      <EventCreation />
+      <QuestCreation />
+    </React.Fragment>
+  )
+}
+
+export default Creation
