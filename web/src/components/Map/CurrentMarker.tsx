31f904e751fa7e2aca5fcc20f2608410ab61c824
hansstanley <nusstuhss@gmail.com>
2022-12-20 11:45:36 +0800

    Change current marker

--- web/src/components/Map/CurrentMarker.tsx
+++ web/src/components/Map/CurrentMarker.tsx
@@ -1,5 +1,8 @@
+import { Person, PersonPin } from '@mui/icons-material';
+import { Avatar, Fab, IconButton, Tooltip } from '@mui/material';
 import { Marker } from 'react-map-gl';
 import { useAppSelector } from '../../features/app/hooks';
+import { auth } from '../../firebase/init';
 
 const CurrentMarker = () => {
 	const { currLocation } = useAppSelector((state) => state.events);
@@ -7,10 +10,15 @@ const CurrentMarker = () => {
 	return currLocation ? (
 		<Marker
 			anchor="bottom"
-			color="red"
 			latitude={currLocation.latitude}
-			longitude={currLocation.longitude}
-		/>
+			longitude={currLocation.longitude}>
+			<Tooltip title="You">
+				<Avatar
+					src={auth.currentUser?.photoURL ?? undefined}
+					sx={{ boxShadow: 10 }}
+				/>
+			</Tooltip>
+		</Marker>
 	) : null;
 };
 
