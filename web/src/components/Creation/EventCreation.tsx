243a087e2b09222ed562475fec99800eec4969b4
hansstanley <nusstuhss@gmail.com>
2022-12-20 08:45:34 +0800

    Add redux state changes to post creation

--- web/src/components/Creation/EventCreation.tsx
+++ web/src/components/Creation/EventCreation.tsx
@@ -1,155 +1,152 @@
-import React, { useState } from "react";
-import { 
-  Box, Button, Grid, Input, TextField, Typography,
-} from "@mui/material";
+import React, { useState } from 'react';
+import { Box, Button, Grid, Input, TextField, Typography } from '@mui/material';
 
-import EventService, { EventCreationData } from "../../services/EventService"
-import { auth } from "../../firebase/init";
-import { GeoPoint } from "firebase/firestore";
-import FormMap from "../Map/FormMap";
-import { useAppSelector } from "../../features/app/hooks";
-
+import { EventCreationData } from '../../services/EventService';
+import { auth } from '../../firebase/init';
+import FormMap from '../Map/FormMap';
+import { useAppDispatch, useAppSelector } from '../../features/app/hooks';
+import { pushSnack } from '../../features/snacks/snacksSlice';
+import { createEvent } from '../../features/events/eventsSlice';
 
 const EventCreation = () => {
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const { markLocation } = useAppSelector((state) => state.events);
+	const dispatch = useAppDispatch();
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const { markLocation } = useAppSelector((state) => state.events);
 
-  const handleTitleChange = (event: any) => {
-    const target = event.target
-    setTitle(target.value)
-  }
+	const handleTitleChange = (event: any) => {
+		const target = event.target;
+		setTitle(target.value);
+	};
 
-  const handleDescriptionChange = (event: any) => {
-    const target = event.target
-    setDescription(target.value)
-  }
+	const handleDescriptionChange = (event: any) => {
+		const target = event.target;
+		setDescription(target.value);
+	};
 
-  const handleEventSubmit = () => {
-    const eventService = new EventService()
-    const uid = auth.currentUser?.uid;
-    
-    if(uid === undefined) {
-      return;
-    }
-    if(markLocation === undefined) {
-      console.log("input lat long")
-      return
-    }
-    const sendEvent = async () => {
-      // prepare data
-      const data: EventCreationData = {
-        user_id: uid,
-        title: title,
-        description: description,
-        timestamp: Date.now().toString(),
-        location: {
-          lat: markLocation.latitude,
-          lng: markLocation.longitude,
-        },
-      }
-      try {
-        await eventService.createEvent(data);
-        console.log("event created")
-      } catch (error) {
-        console.log(error);
-      }
-    }
+	const handleEventSubmit = () => {
+		const uid = auth.currentUser?.uid;
+		if (uid === undefined) {
+			dispatch(
+				pushSnack({ severity: 'error', message: 'You are not logged in!' }),
+			);
+			return;
+		}
+		if (markLocation === undefined) {
+			dispatch(
+				pushSnack({ severity: 'error', message: 'Please choose a location!' }),
+			);
+			return;
+		}
 
-    sendEvent()
-  }
+		// prepare data
+		const data: EventCreationData = {
+			user_id: uid,
+			title: title,
+			description: description,
+			timestamp: Date.now().toString(),
+			location: {
+				lat: markLocation.latitude,
+				lng: markLocation.longitude,
+			},
+		};
 
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        maxWidth: "1200px",
-        width: "80%",
-        margin: "0 auto",
-        flexDirection: "column",
-        alignItems: "left",
-        justifyContent: "space-between",
-        padding: "1rem",
-        border: 2,
-        gap: "1rem",
-      }}
-    >
+		dispatch(createEvent(data))
+			.then((_) =>
+				dispatch(pushSnack({ severity: 'success', message: 'Event created!' })),
+			)
+			.catch((err) => {
+				console.error(err);
+				dispatch(
+					pushSnack({ severity: 'error', message: 'Error creating event!' }),
+				);
+			});
+	};
 
-      <Box>
-        <Typography
-          sx={{
-            alignSelf: "flex-start",
-            fontFamily:"Roboto",
-            fontSize: "18px",
-          }}
-        >
-          Title
-        </Typography>
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				maxWidth: '1200px',
+				width: '80%',
+				margin: '0 auto',
+				flexDirection: 'column',
+				alignItems: 'left',
+				justifyContent: 'space-between',
+				padding: '1rem',
+				border: 2,
+				gap: '1rem',
+			}}>
+			<Box>
+				<Typography
+					sx={{
+						alignSelf: 'flex-start',
+						fontFamily: 'Roboto',
+						fontSize: '18px',
+					}}>
+					Title
+				</Typography>
 
-        <TextField
-          onChange={handleTitleChange}
-          sx={{
-            width: "100%"
-          }}
-          />
-      </Box>
+				<TextField
+					onChange={handleTitleChange}
+					sx={{
+						width: '100%',
+					}}
+				/>
+			</Box>
 
-      <Box>
-        <Typography 
-          sx={{
-            alignSelf: "flex-start",
-            fontFamily:"Roboto",
-            fontSize: "18px",
-          }}
-        >
-          Description
-        </Typography>
-        <TextField 
-          onChange={handleDescriptionChange}
-            multiline
-            minRows={4}
-          sx={{
-            width: "100%",
-          }}
-        />
-      </Box>
+			<Box>
+				<Typography
+					sx={{
+						alignSelf: 'flex-start',
+						fontFamily: 'Roboto',
+						fontSize: '18px',
+					}}>
+					Description
+				</Typography>
+				<TextField
+					onChange={handleDescriptionChange}
+					multiline
+					minRows={4}
+					sx={{
+						width: '100%',
+					}}
+				/>
+			</Box>
 
-      <Box>
-        <Typography
-          sx={{
-            alignSelf: "flex-start",
-            fontFamily:"Roboto",
-            fontSize: "18px",
-          }}
-        >
-          Location
-        </Typography>
-        <FormMap />
-      </Box>
+			<Box>
+				<Typography
+					sx={{
+						alignSelf: 'flex-start',
+						fontFamily: 'Roboto',
+						fontSize: '18px',
+					}}>
+					Location
+				</Typography>
+				<FormMap />
+			</Box>
 
-      <Button
-        onClick={handleEventSubmit}
-        sx={{
-          border: 1,
-          width: 200,
-          alignSelf: "center",
-          // borderRadius: 100,
-          borderColor: "black",
-          marginTop: "1rem", 
-          color: "black",
-          borderRadius: "0",
-          marginLeft: "1rem",
-          textTransform: "none",
-          fontSize: "1rem",
-          fontWeight: "600",
-          letterSpacing: "1px"
+			<Button
+				onClick={handleEventSubmit}
+				sx={{
+					border: 1,
+					width: 200,
+					alignSelf: 'center',
+					// borderRadius: 100,
+					borderColor: 'black',
+					marginTop: '1rem',
+					color: 'black',
+					borderRadius: '0',
+					marginLeft: '1rem',
+					textTransform: 'none',
+					fontSize: '1rem',
+					fontWeight: '600',
+					letterSpacing: '1px',
+				}}>
+				Create Event
+			</Button>
+		</Box>
+	);
+};
 
-        }}
-      >
-        Create Event
-      </Button>
-    </Box>
-  )
-}
-
-export default EventCreation
+export default EventCreation;
