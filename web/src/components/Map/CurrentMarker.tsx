194d646ec10c9411a1bc3fc8af2482ace51e0493
Connor Lim <dev@connorlim.net>
2022-12-20 12:27:41 +0800

    Add better current location marker

--- web/src/components/Map/CurrentMarker.tsx
+++ web/src/components/Map/CurrentMarker.tsx
@@ -1,30 +1,29 @@
-import { Avatar, Badge, Tooltip } from '@mui/material';
-import { Marker } from 'react-map-gl';
-import { useAppSelector } from '../../features/app/hooks';
-import { auth } from '../../firebase/init';
+import { Avatar, Badge, Tooltip } from "@mui/material";
+import { Marker, Popup } from "react-map-gl";
+import { useAppSelector } from "../../features/app/hooks";
+import { auth } from "../../firebase/init";
 
 const CurrentMarker = () => {
-	const { currLocation } = useAppSelector((state) => state.events);
+  const { currLocation } = useAppSelector((state) => state.events);
 
-	return currLocation ? (
-		<Marker
-			anchor="bottom"
-			latitude={currLocation.latitude}
-			longitude={currLocation.longitude}>
-			<Tooltip title="You">
-				<Badge
-					overlap="circular"
-					color="success"
-					anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-					variant="dot">
-					<Avatar
-						src={auth.currentUser?.photoURL ?? undefined}
-						sx={{ boxShadow: 10 }}
-					/>
-				</Badge>
-			</Tooltip>
-		</Marker>
-	) : null;
+  return currLocation ? (
+    <>
+      <Popup
+        longitude={currLocation.longitude}
+        latitude={currLocation.latitude}
+        anchor="top"
+        closeButton={false}
+        closeOnClick={false}
+      >
+        You are here
+      </Popup>
+      <Marker
+        anchor="bottom"
+        latitude={currLocation.latitude}
+        longitude={currLocation.longitude}
+      ></Marker>
+    </>
+  ) : null;
 };
 
 export default CurrentMarker;
