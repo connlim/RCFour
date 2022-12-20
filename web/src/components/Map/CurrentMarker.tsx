f5e68d20e9245459bfde8dc27842c8b23d19542d
Connor Lim <dev@connorlim.net>
2022-12-20 12:08:02 +0800

    Add label to location marker

--- web/src/components/Map/CurrentMarker.tsx
+++ web/src/components/Map/CurrentMarker.tsx
@@ -1,17 +1,28 @@
-import { Marker } from 'react-map-gl';
-import { useAppSelector } from '../../features/app/hooks';
+import { Marker, Popup } from "react-map-gl";
+import { useAppSelector } from "../../features/app/hooks";
 
 const CurrentMarker = () => {
-	const { currLocation } = useAppSelector((state) => state.events);
+  const { currLocation } = useAppSelector((state) => state.events);
 
-	return currLocation ? (
-		<Marker
-			anchor="bottom"
-			color="red"
-			latitude={currLocation.latitude}
-			longitude={currLocation.longitude}
-		/>
-	) : null;
+  return currLocation ? (
+    <>
+      <Popup
+        longitude={currLocation.longitude}
+        latitude={currLocation.latitude}
+        anchor="top"
+        closeButton={false}
+        closeOnClick={false}
+      >
+        You are here
+      </Popup>
+      <Marker
+        anchor="bottom"
+        color="red"
+        latitude={currLocation.latitude}
+        longitude={currLocation.longitude}
+      />
+    </>
+  ) : null;
 };
 
 export default CurrentMarker;
